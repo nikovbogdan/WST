5f2eadc48912a9ee2cf116b5d654de35bebc81b4
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-12-22 15:24:23 +0200

    Create\Delete Test

---
+++ src/test/java/tests/ItemAPITest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import api.ItemAPI;
+import api.LoginAPI;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.Item;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ItemAPITest {
+
+    private static final String TOKEN = LoginAPI.getToken();
+    private final static ItemAPI itemAPI = new ItemAPI(TOKEN);
+    private Item item;
+
+    @BeforeEach
+    public void beforeEach() {
+        item = Item.builder()
+                .name("Nescafe")
+                .price(20.50)
+                .priceForQuantity(5.0)
+                .currency("BGN")
+                .unit("kg.")
+                .build();
+
+    }
+
+
+    @Test
+    @DisplayName("Create/Delete item")
+    public void creatingAndDeletingItem() {
+
+        Response createResp = itemAPI.createItem(item);
+        assertEquals(201, createResp.statusCode());
+        int id = createResp.then().extract().path("id");
+
+        Response deleteResp = itemAPI.deleteItem(id);
+        assertEquals(204,deleteResp.statusCode());
+    }
+
+
+}
